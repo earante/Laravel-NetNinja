fea0920e599742ec6a8362888b5edf88a15e4635
earante <arante.emmanuel2017@gmail.com>
2025-10-27 08:03:12 +0800

    feat: end of part 2 video tutorial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja | Network</title>
+</head>
+<body>
+    <h1>Welcome to Ninja Network!</h1>
+    <p>Click the button below to view the list of Ninjas.</p>
+    <a href="/ninjas" class="btn">Find Ninjas.</a>
+</body>
+</html>
